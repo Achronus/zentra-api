225ae89f2e0984e4d5e8a843ae37ae3d1db60f69
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-31 22:12:41 +0100

    refactor(db): add `DB` prefix to DB model class names for clarity

--- zentra_api/cli/template/app/models.py
+++ zentra_api/cli/template/app/models.py
@@ -6,7 +6,7 @@ from .config import SETTINGS
 from zentra_api.crud import CRUD, UserCRUD
 
 
-class User(SETTINGS.SQL.Base):
+class DBUser(SETTINGS.SQL.Base):
     """A model of the `User` table."""
 
     __tablename__ = "users"
@@ -17,16 +17,16 @@ class User(SETTINGS.SQL.Base):
     is_active = Column(Boolean, default=True)
 
 
-class UserDetails(SETTINGS.SQL.Base):
+class DBUserDetails(SETTINGS.SQL.Base):
     """A model of the `UserDetails` table."""
 
     __tablename__ = "user_details"
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    email = Column(String, unique=True)
-    phone = Column(String, unique=True)
-    full_name = Column(String)
+    email = Column(String, unique=True, default=None)
+    phone = Column(String, unique=True, default=None)
+    full_name = Column(String, default=None)
 
 
 SETTINGS.SQL.create_all()
@@ -36,8 +36,8 @@ class DBConnections:
     """A place to store all table CRUD operations."""
 
     def __init__(self) -> None:
-        self.user = UserCRUD(model=User)
-        self.user_details = CRUD(model=UserDetails)
+        self.user = UserCRUD(model=DBUser)
+        self.user_details = CRUD(model=DBUserDetails)
 
 
 CONNECT = DBConnections()
