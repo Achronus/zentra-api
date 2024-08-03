871a869d0a5654ddadac72d5903bd3b9bb7224b3
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-08-03 10:30:39 +0100

    refactor(auth): Added `Field` declarations to model attributes for docs

--- zentra_api/cli/template/app/auth/schema.py
+++ zentra_api/cli/template/app/auth/schema.py
@@ -1,25 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
-    username: str
+    username: str = Field(..., description="A unique username to identify the user")
 
 
 class CreateUser(UserBase):
-    password: str
-    is_active: bool = True
+    password: str = Field(
+        ..., description="The users password to login to the platform"
+    )
+    is_active: bool = Field(default=True, description="The users account status")
 
 
 class UserDetails(BaseModel):
-    email: str | None = None
-    phone: str | None = None
-    full_name: str | None = None
-    is_active: bool
+    email: str | None = Field(default=None, description="The users email address")
+    phone: str | None = Field(default=None, description="The users contact number")
+    full_name: str | None = Field(default=None, description="The users full name")
+    is_active: bool = Field(..., description="The users account status")
 
 
 class GetUser(UserBase, UserDetails):
     pass
-
-
-class UserInDB(GetUser):
-    password: str
