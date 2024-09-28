0a0541c453b6527db62f1032e447de2a4c2cbb79
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-09-28 13:10:39 +0100

    docs(reference): Updated `auth` module docstrings for clarity.

--- zentra_api/auth/context.py
+++ zentra_api/auth/context.py
@@ -3,25 +3,49 @@ Custom hashing contexts for Zentra API projects.
 """
 
 import bcrypt
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class BcryptContext(BaseModel):
     """
     A custom context for bcrypt hashing.
 
+    ??? example "Example Usage"
+        ```python
+        from zentra_api.auth.context import BcryptContext
+
+        bcrypt_context = BcryptContext(rounds=14)
+        ```
+
     Parameters:
-        rounds (integer): the computational cost factor for hashing. Defaults to 12 (optional).
+        rounds (int): (optional) the computational cost factor for hashing.
+
+            Higher rounds increase security but also slow down the hashing process, making brute-force attacks more difficult.
+
+            Defaults to `12`.
     """
 
-    rounds: int = 12
+    rounds: int = Field(
+        default=12, description="The computational cost factor for hashing."
+    )
 
     def hash(self, password: str) -> str:
         """
-        Hashes a password. Returns the hashed password.
+        Hashes a password and returns the new hashed password.
+
+        ??? example "Example Usage"
+            ```python
+            from zentra_api.auth.context import BcryptContext
+
+            bcrypt_context = BcryptContext()
+            hashed = bcrypt_context.hash('secretpassword')
+            ```
 
         Parameters:
-            password (string): the plain password to hash
+            password (str): the plain password to hash
+
+        Returns:
+            str: The bcrypt's hexadecimal representation of the salt and hashed version of the password.
         """
         salt = bcrypt.gensalt(rounds=self.rounds)
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
@@ -29,10 +53,26 @@ class BcryptContext(BaseModel):
 
     def verify(self, password: str, hashed_password: str) -> bool:
         """
-        Verifies a password against a given hash. Returns True if the password matches, False otherwise.
+        Verifies a password against a given hash.
+
+        Returns True if the password matches, False otherwise.
+
+        ??? example "Example Usage"
+            ```python
+            from zentra_api.auth.context import BcryptContext
+
+            bcrypt_context = BcryptContext()
+            verify = bcrypt_context.verify(
+                password='secretpassword',
+                hashed_password='$2b$12$vXZLwyt45QGS7C5V5ysA2eNtyArSa9RThVzi9erupyi23ThLi628i'
+            )
+            ```
 
         Parameters:
             password (string): the plain password to verify
             hashed_password (string): The hashed password to verify against
+
+        Returns:
+           bool : True if the given plain text matches, False otherwise.
         """
         return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
--- zentra_api/auth/security.py
+++ zentra_api/auth/security.py
@@ -19,6 +19,14 @@ class SecurityUtils(BaseModel):
     """
     Contains utility methods for managing user authentication.
 
+    ??? example "Example Usage"
+        ```python
+        from app.core.config import SETTINGS
+        from zentra_api.auth.security import SecurityUtils
+
+        security = SecurityUtils(auth=SETTINGS.AUTH)
+        ```
+
     Parameters:
         auth (AuthConfig): a Zentra API AuthConfig model
     """
@@ -27,7 +35,16 @@ class SecurityUtils(BaseModel):
 
     def hash_password(self, password: str) -> str:
         """
-        Uses the pwd_context to hash a password.
+        Uses the [BcryptContext](/api/reference/auth/context/#zentra_api.auth.context.BcryptContext) to hash a password.
+
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            hashed_pass = security.hash_password("supersecretpassword")
+            ```
 
         Parameters:
             password (str): a plain text password
@@ -39,7 +56,19 @@ class SecurityUtils(BaseModel):
 
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
         """
-        Uses the pwd_context to verify a password.
+        Uses the [BcryptContext](/api/reference/auth/context/#zentra_api.auth.context.BcryptContext) to verify a password.
+
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            result = security.verify_password(
+                plain_password="supersecretpassword",
+                hashed_password="nelC0DeSokMRpjuCix70g-28BCMRqrrEgDRO5xBF0mY"
+            )
+            ```
 
         Parameters:
             plain_password (str): a plain text password
@@ -54,11 +83,37 @@ class SecurityUtils(BaseModel):
         self, token_type: TokenTypeLiteral, expires_delta: timedelta | None = None
     ) -> datetime:
         """
-        Creates an expiration datetime object for a JWT token. If `expires_delta=None`, automatically generates one using environment variables.
+        Creates an expiration datetime object for a JWT token.
+
+        If `expires_delta=None`, automatically generates one using environment variables.
+
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            expiry = security.expiration('access')
+            ```
 
         Parameters:
-            token_type (TokenTypeLiteral): the type of token to create. Options: `['access', 'refresh']`
-            expires_delta (timedelta): a timedelta object representing the expiration time
+            token_type (TokenTypeLiteral): the type of token to create.
+
+                Options: `access`, `refresh`
+
+            expires_delta (timedelta): (optional) a timedelta object representing the expiration time. Can be manually provided. `None` by default.
+
+                If `None`, it will use environment variables to calculate an appropriate duration based on token type.
+
+                Defaults when `None`:
+
+                - `access` token: 15 mins
+                - `refresh` token: 7 days
+
+                Environment variables to update token expiry length:
+
+                - `access`: `AUTH__ACCESS_TOKEN_EXPIRE_MINS`
+                - `refresh`: `AUTH__REFRESH_TOKEN_EXPIRE_MINS`
 
         Returns:
             datetime: a datetime object representing the expiration time
@@ -70,10 +125,34 @@ class SecurityUtils(BaseModel):
 
     def expire_mins(self, token_type: TokenTypeLiteral) -> timedelta:
         """
-        Returns the token expire minutes as a timedelta.
+        Returns the token expire minutes as a timedelta using environment variables.
+
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            expiry = security.expire_mins('access')
+            ```
 
         Parameters:
-            token_type (TokenTypeLiteral): the type of token to create. Options: `['access', 'refresh']`
+            token_type (TokenTypeLiteral): the type of token to create.
+
+                Options: `access`, `refresh`.
+
+                Environment variables to update token expiry length:
+
+                - `access`: `AUTH__ACCESS_TOKEN_EXPIRE_MINS`
+                - `refresh`: `AUTH__REFRESH_TOKEN_EXPIRE_MINS`
+
+                If not set, duration defaults:
+
+                - `access` token: 15 mins
+                - `refresh` token: 7 days
+
+
+
 
         Returns:
             timedelta: a timedelta object representing the expiration time
@@ -88,6 +167,36 @@ class SecurityUtils(BaseModel):
     def encrypt(self, model: BaseModel, attributes: str | list[str]) -> BaseModel:
         """
         Encrypts a set of data in a model and returns it as a new model.
+
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            from pydantic import BaseModel, Field
+
+
+            class UserBase(BaseModel):
+                username: str = Field(..., description="A unique username to identify the user")
+
+
+            class CreateUser(UserBase):
+                password: str = Field(
+                    ..., description="The users password to login to the platform"
+                )
+                is_active: bool = Field(default=True, description="The users account status")
+
+
+            user = CreateUser(username="admin", password="supersecret")
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+
+            # Example 1
+            encrypted_user: CreateUser = security.encrypt(user, "password")
+
+            # Example 2
+            encrypted_user: CreateUser = security.encrypt(user, ["username", "password"])
+            ```
 
         Parameters:
             model (BaseModel): a Pydantic BaseModel object
@@ -123,8 +232,23 @@ class SecurityUtils(BaseModel):
         Parameters:
             data (dict): a dictionary of data to encrypt
             secret_key (str): the secret key to use for encryption
-            token_type (TokenTypeLiteral): the type of token to create. Options: `['access', 'refresh']`
-            expires_delta (timedelta): a timedelta object representing the expiration time
+            token_type (TokenTypeLiteral): the type of token to create.
+
+                Options: `['access', 'refresh']`
+
+            expires_delta (timedelta): (optional) a timedelta object representing the expiration time. Can be manually provided. `None` by default.
+
+                If `None`, it will use environment variables to calculate an appropriate duration based on token type.
+
+                Defaults when `None`:
+
+                - `access` token: 15 mins
+                - `refresh` token: 7 days
+
+                Environment variables to update token expiry length:
+
+                - `access`: `AUTH__ACCESS_TOKEN_EXPIRE_MINS`
+                - `refresh`: `AUTH__REFRESH_TOKEN_EXPIRE_MINS`
 
         Returns:
             str: a JWT token
@@ -146,9 +270,22 @@ class SecurityUtils(BaseModel):
         """
         Creates a JWT access token for the given data and returns it.
 
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            security.create_access_token({"sub": "admin"})
+            ```
+
         Parameters:
             data (dict): a dictionary of data to encrypt
-            expires_delta (timedelta): a timedelta object representing the expiration time
+            expires_delta (timedelta): (optional) a timedelta object representing the expiration time. Can be manually provided. `None` by default.
+
+                If `None`, it will use the environment variable `AUTH__ACCESS_TOKEN_EXPIRE_MINS` to calculate the access token duration.
+
+                If `None` and no `AUTH__ACCESS_TOKEN_EXPIRE_MINS`, duration is set to `15 mins`.
 
         Returns:
             str: a JWT access token
@@ -166,9 +303,22 @@ class SecurityUtils(BaseModel):
         """
         Creates a JWT refresh token for the given data and returns it.
 
+        ??? example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            security.create_refresh_token({"sub": "admin"})
+            ```
+
         Parameters:
             data (dict): a dictionary of data to encrypt
-            expires_delta (timedelta): a timedelta object representing the expiration time
+            expires_delta (timedelta): (optional) a timedelta object representing the expiration time. Can be manually provided. `None` by default.
+
+                If `None`, it will use the environment variable `AUTH__REFRESH_TOKEN_EXPIRE_MINS` to calculate the refresh token duration.
+
+                If `None` and no `AUTH__REFRESH_TOKEN_EXPIRE_MINS`, duration is set to `7 days`.
 
         Returns:
             str: a JWT refresh token
@@ -209,7 +359,20 @@ class SecurityUtils(BaseModel):
 
     def verify_access_token(self, token: str) -> str:
         """
-        Verifies the validity of a JWT access token. Returns the token data if valid.
+        Verifies the validity of a JWT access token based on the `AUTH__SECRET_ACCESS_KEY` environment variable.
+
+        Returns the token data if valid.
+
+        ??? Example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            security.verify_access_token(
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjoxNzI3NTI0MzMwfQ.cX0COXJkSYTtcbS9YIRhv0L9f4YhzWSxmD_mqZJ64IE"
+            )
+            ```
 
         Parameters:
             token (str): a JWT access token
@@ -221,7 +384,20 @@ class SecurityUtils(BaseModel):
 
     def verify_refresh_token(self, token: str) -> str:
         """
-        Verifies the validity of a JWT refresh token. Returns the token data if valid.
+        Verifies the validity of a JWT refresh token based on the `AUTH__SECRET_REFRESH_KEY` environment variable.
+
+        Returns the token data if valid.
+
+        ??? Example "Example Usage"
+            ```python
+            from app.core.config import SETTINGS
+            from zentra_api.auth.security import SecurityUtils
+
+            security = SecurityUtils(auth=SETTINGS.AUTH)
+            security.verify_refresh_token(
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjoxNzI4MTI4MjMwfQ.PUB5aAuyMwyAN4erInj1vvZQT5sv50emQxJ8lrDMkVc"
+            )
+            ```
 
         Parameters:
             token (str): a JWT refresh token
